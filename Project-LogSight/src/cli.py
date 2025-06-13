881f1b7aa35cf0902b2f1ddcb4ca8a55ff736168
Sujahath Siddeek <sujahathmhmd@gmail.com>
2025-06-13 12:24:11 +0530

    Coded the CLI

--- Project-LogSight/src/cli.py
+++ Project-LogSight/src/cli.py
@@ -1,9 +1,24 @@
 import argparse
-from parser import parse_
+from parser import parse_file
+from summarizer import summarize
+
 
 
 def main():
     print("LogSight")
+    p = argparse.ArgumentParser()
+    p.add_argument("files", nargs="+", help="Log files to analyse")
+    args = p.parse_args()
+
+    all_entries = []
+
+    for fp in args.files:
+        all_entries.extend(parse_file(fp))
+    
+    counts = summarize(all_entries)
+    print("LogSight Summary: ")
+    for lvl, cnt in counts.items():
+        print(f"{lvl} : {cnt}")
 
 if (__name__ == "__main__"):
     main()
